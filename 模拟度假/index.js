af439434b92a17f98ff429131186add7910aacf8
bcm-ray_crazy <975875040@qq.com>
2020-07-15 22:32:41 +0800

    add file

---
+++ 模拟度假/index.js
@@ -0,0 +1,19 @@
+//设置Cookie
+function setCookie(name, value) {
+    var Days = 1;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString() + ";path:/";
+}
+/*
+ 获取Cookie中的值
+ */
+function getCookie(objName) {//获取指定名称的cookie的值
+    var arrStr = document.cookie.split("; ");
+    for (var i = 0; i < arrStr.length; i++) {
+        var temp = arrStr[i].split("=");
+        if (temp[0] == objName) {
+            return decodeURI(temp[1]);
+        }
+    }
+}
